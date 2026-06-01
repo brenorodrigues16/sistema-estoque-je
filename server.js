6e6ed5681aae87216600e43eca61cc3e35aa8f24
Breno Ribeiro <breno.rodriguesdeadnow@gmail.com>
2026-06-01 14:33:00 -0300

    ajustes URLs

--- server.js
+++ server.js
@@ -32,6 +32,11 @@ app.use(express.json());
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+// - - ROTA PRINCIPAL (PÁGINA INICIAL) - - //
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
 
 
 app.get('/api/teste-db', async (req, res)=>{
