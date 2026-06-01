af43bef65f9035d545d0e154c80f19a87822bb90
Breno Ribeiro <breno.rodriguesdeadnow@gmail.com>
2026-06-01 15:04:37 -0300

    ajustando o caminho para docs

--- server.js
+++ server.js
@@ -30,11 +30,11 @@ app.use(cors());
 
 app.use(express.json());
 
-app.use(express.static(path.join(__dirname, 'documentos')));
+app.use(express.static(path.join(__dirname, 'docs')));
 
 // - - ROTA PRINCIPAL (PÁGINA INICIAL) - - //
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'documentos', 'index.html'));
+    res.sendFile(path.join(__dirname, 'docs', 'index.html'));
 });
 
 
